976bff13d0c5ceab6909e1e6ac59fc14e54350ed
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-06-29 15:15:35 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -97,19 +97,17 @@ document.onkeydown = function(e) {
   }
 }
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/648defbbcc26a871b0232a41/1h3570tg3';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+// Start of Tawk.to Live Chat
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+//   (function(){
+//   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+//   s1.async=true;
+//   s1.src='https://embed.tawk.to/648defbbcc26a871b0232a41/1h3570tg3';
+//   s1.charset='UTF-8';
+//   s1.setAttribute('crossorigin','*');
+//   s0.parentNode.insertBefore(s1,s0);
+//   })();
+// End of Tawk.to Live Chat
 
 async function fetchData(type = "skills") {
     let response
